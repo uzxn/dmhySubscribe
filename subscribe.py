38f95a08df844a82b43fbc4111a37884c1beb5a0
Zhixiang <hzxhub@qq.com>
2023-02-08 22:11:51 +0800

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -2,13 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 with open("index.html", "w", encoding="utf-8") as page:
+    page.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
+    page.write("<h1>Index of Subscribes</h1>\n")
     with open("subscribes.txt", "r", encoding="utf-8") as sub:
         url = sub.readline()
         while url:
             text = requests.get(url).text
             soup = BeautifulSoup(text, "xml")
-            page.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
-            page.write("<h1>Index of Subscribes</h1>\n")
             page.write(f"<a href='{soup.title.string}.html'>{soup.title.string}</a><br>\n")
             with open(f"{soup.title.string}.html", "w", encoding="utf-8") as f:
                 f.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
