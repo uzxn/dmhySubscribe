e9875b5e71af073348b86229d4bfca6f18eabbbb
Zhixiang <hzxhub@qq.com>
2023-02-08 21:46:07 +0800

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -1,15 +1,17 @@
-import sys
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://dmhy.b168.net/topics/rss/team_id/650/rss.xml"
-text = requests.get(url).text
-soup = BeautifulSoup(text, "xml")
-
-with open(f"650.html", "w", encoding="utf-8") as f:
-    f.write("<div style='font-size: 75%'>\n")
-    f.write(f"<h1>{soup.title.string}</h1>\n")
-    for i in soup.select("item"):
-        f.write(f"<code>{i.pubDate.string}</code>&emsp;\n")
-        f.write(f"<a href='{i.enclosure['url']}'>{i.title.string}</a><br>\n")
-    f.write("</div>")
+with open("index.html", "w", encoding="utf-8") as page:
+    with open("subscribes.txt", "r"， encoding="utf-8") as sub:
+        url = sub.readline()
+        while url:
+            text = requests.get(url).text
+            soup = BeautifulSoup(text, "xml")
+            page.write("<h1>Index of Subscribes</h1>\n")
+            page.write(f"<a href='{soup.title.string}.html'>{soup.title.string}</a><br>\n")
+            with open(f"{soup.title.string}.html", "w", encoding="utf-8") as f:
+                f.write(f"<h1>{soup.title.string}</h1>\n")
+                for i in soup.select("item"):
+                    f.write(f"{i.pubDate.string}\n")
+                    f.write(f"<a href='{i.enclosure['url']}'>{i.title.string}</a><br>\n")
+            url = sub.readline()
