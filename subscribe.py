e41dc65590184731b1a27bd1565144bfb2d159d0
Zhixiang <hzxhub@qq.com>
2023-02-08 21:59:26 +0800

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -7,13 +7,13 @@ with open("index.html", "w", encoding="utf-8") as page:
         while url:
             text = requests.get(url).text
             soup = BeautifulSoup(text, "xml")
-            page.write("<meta name='viewport' content='width=device-width initial-scale=1'>")
+            page.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
             page.write("<h1>Index of Subscribes</h1>\n")
             page.write(f"<a href='{soup.title.string}.html'>{soup.title.string}</a><br>\n")
             with open(f"{soup.title.string}.html", "w", encoding="utf-8") as f:
-                f.write("<meta name='viewport' content='width=device-width initial-scale=1'>")
+                f.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
                 f.write(f"<h1>{soup.title.string}</h1>\n")
                 for i in soup.select("item"):
-                    f.write(f"{i.pubDate.string}\n")
+                    f.write(f"<code>{i.pubDate.string}</code>\n")
                     f.write(f"<a href='{i.enclosure['url']}'>{i.title.string}</a><br>\n")
             url = sub.readline()
