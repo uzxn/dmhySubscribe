e5981d2df2b994c48df9ae027c5a4f570558f622
Zhixiang <hzxhub@qq.com>
2023-02-10 02:56:48 +0800

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -7,9 +7,11 @@ with open("index.html", "w", encoding="utf-8") as page:
     page.write("<h1>Index of Subscribes</h1>\n")
     with open("subscribes.txt", "r", encoding="utf-8") as sub:
         for url in sub.readlines():
+            j = 1
             feed = feedparser.parse(url, agent="Mozilla/5.0 (Android 8.1.0; Mobile; rv:109.0) Gecko/109.0 Firefox/109.0")
-            page.write(f"<a href='{feed.feed.title}.html'>{feed.feed.title}</a><br>\n")
-            with open(f"{feed.feed.title}.html", "w", encoding="utf-8") as f:
+            page.write(f"<a href='{j}.html'>{feed.feed.title}</a><br>\n")
+            with open(f"{j}.html", "w", encoding="utf-8") as f:
                 f.write(f"<h1>{feed.feed.title}</h1>\n")
                 for i in feed.entries:
                     f.write(f"<a href='{i.enclosures[0].href}'>{i.title}</a><br>\n")
+            j = j + 1
