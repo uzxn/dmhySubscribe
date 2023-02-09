d92b82a5e3fc816141e1068d18b568a5881eb31f
Zhixiang <hzxhub@qq.com>
2023-02-09 20:55:30 +0800

    update

--- subscribe.py
+++ subscribe.py
@@ -1,19 +1,31 @@
-import requests
-from bs4 import BeautifulSoup
+import time
+import feedparser
 
 with open("index.html", "w", encoding="utf-8") as page:
-    page.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
-    page.write("<h1>Index of Subscribes</h1>\n")
+    page.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n"
+               "<link rel='stylesheet' href='https://unpkg.com/mvp.css@1.12/mvp.css'>\n"
+               "<main>\n"
+               "<h1>Index of Subscribes</h1>\n"
+               "<table>\n")
     with open("subscribes.txt", "r", encoding="utf-8") as sub:
         url = sub.readline()
-        while url:
-            text = requests.get(url).text
-            soup = BeautifulSoup(text, "xml")
-            page.write(f"<a href='{soup.title.string}.html'>{soup.title.string}</a><br>\n")
-            with open(f"{soup.title.string}.html", "w", encoding="utf-8") as f:
-                f.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
-                f.write(f"<h1>{soup.title.string}</h1>\n")
-                for i in soup.select("item"):
-                    f.write(f"<code>{i.pubDate.string}</code>\n")
-                    f.write(f"<a href='{i.enclosure['url']}'>{i.title.string}</a><br>\n")
-            url = sub.readline()
+            while url:
+                feed = feedparser.parse(url)
+                page.write(f"<tr>\n"
+                           "<th>{time.strftime('%Y-%m-%d %H:%M:%S', feed.feed.published_parsed)}</th>\n"
+                           "<th><a href='{feed.feed.title}.html'>{feed.feed.title}</a></th>\n"
+                           "</tr>\n")
+                with open(f"{feed.feed.title}.html", "w", encoding="utf-8") as f:
+                    f.write(f"<meta name='viewport' content='width=device-width initial-scale=1'>\n"
+                            "<link rel='stylesheet' href='https://unpkg.com/mvp.css@1.12/mvp.css'>\n"
+                            "<main>\n"
+                            "<h1>{feed.feed.title}</h1>\n"
+                            "<table>\n")
+                    for i in feed.entries:
+                        f.write(f"<tr>\n"
+                                "<th>{time.strftime('%Y-%m-%d %H:%M:%S', i.published_parsed)}</th>\n"
+                                "<th>{i.enclosures[0].href}</th>\n"
+                                "</tr>\n")
+                    f.write("</trable>\n</main>")
+    page.write("</trable>\n</main>")
+    url = sub.readline()
