7d4502b6d94fb4c00b34cb2885f24672fd3cc1f8
Zhixiang <hzxhub@qq.com>
2023-02-09 21:05:47 +0800

    update

--- subscribe.py
+++ subscribe.py
@@ -16,13 +16,13 @@ with open("index.html", "w", encoding="utf-8") as page:
             page.write(f"<th><a href='{feed.feed.title}.html'>{feed.feed.title}</a></th>\n")
             page.write("</tr>\n")
             with open(f"{feed.feed.title}.html", "w", encoding="utf-8") as f:
-                f.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n"
+                f.write("<meta name='viewport' content='width=device-width initial-scale=1'>\n")
                 f.write("<link rel='stylesheet' href='https://unpkg.com/mvp.css@1.12/mvp.css'>\n")
                 f.write("<main>\n")
                 f.write(f"<h1>{feed.feed.title}</h1>\n")
                 f.write("<table>\n")
                 for i in feed.entries:
-                    f.write("<tr>\n"
+                    f.write("<tr>\n")
                     f.write(f"<th>{time.strftime('%Y-%m-%d %H:%M:%S', i.published_parsed)}</th>\n")
                     f.write(f"<th>{i.enclosures[0].href}</th>\n")
                     f.write("</tr>\n")
